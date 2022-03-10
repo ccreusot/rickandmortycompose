cc74a0f0fc014e65527acd2bc59442f231f236e4
Cedric Creusot <cedric.creusot@gmail.com>
2022-03-10 08:55:59 +0100

    add characters view model test

--- app/src/main/java/fr/cedriccreusot/rickandmortyapi/domain/viewmodels/CharactersViewModel.kt
+++ app/src/main/java/fr/cedriccreusot/rickandmortyapi/domain/viewmodels/CharactersViewModel.kt
@@ -1,9 +1,10 @@
 package fr.cedriccreusot.rickandmortyapi.domain.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import fr.cedriccreusot.rickandmortyapi.domain.repositories.CharacterRepository
+import kotlinx.coroutines.launch
 
 class CharactersViewModel(private val repository: CharacterRepository) : ViewModel() {
-
 
 }
---
+++ app/src/test/java/fr/cedriccreusot/rickandmortyapi/domain/viewmodels/CharactersViewModelTest.kt
@@ -0,0 +1,9 @@
+package fr.cedriccreusot.rickandmortyapi.domain.viewmodels
+
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+
+class CharactersViewModelTest : ShouldSpec({
+
+
+})
